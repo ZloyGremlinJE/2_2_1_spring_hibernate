e28052cf8375ca4779f27e8667d819196d378c6f
Max Ivanov <rellepro@gmail.com>
2021-01-27 21:40:32 +0300

    Solution for check

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -10,41 +10,31 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class MainApp {
-   public static void main(String[] args) throws SQLException {
-      AnnotationConfigApplicationContext context = 
-            new AnnotationConfigApplicationContext(AppConfig.class);
+    public static void main(String[] args) throws SQLException {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
 
-      UserService userService = context.getBean(UserService.class);
+        UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+        userService.add(new User("User5", "Lastname5", "user5@mail.ru", new Car("Лада", 2112)));
+        userService.add(new User("User6", "Lastname6", "user6@mail.ru", new Car("Волга", 21)));
+        userService.add(new User("User7", "Lastname7", "user7@mail.ru", new Car("BMW", 525)));
+        userService.add(new User("User8", "Lastname8", "user8@mail.ru", new Car("Porshe", 916)));
 
-      List<User> users = userService.listUsers();
-//      for (User user : users) {
-//         System.out.println("Id = "+user.getId());
-//         System.out.println("First Name = "+user.getFirstName());
-//         System.out.println("Last Name = "+user.getLastName());
-//         System.out.println("Email = "+user.getEmail());
-//         System.out.println();
-//      }
+        List<User> users = userService.listUsers();
+        for (User user : users) {
+            System.out.println("Id = " + user.getId());
+            System.out.println("First Name = " + user.getFirstName());
+            System.out.println("Last Name = " + user.getLastName());
+            System.out.println("Email = " + user.getEmail());
+            System.out.println("Car = " + user.getCar().getModel() + " Series= " + user.getCar().getSeries());
+            System.out.println();
+        }
 
-      userService.add(new User("User5", "Lastname5", "user5@mail.ru", new Car("Лада", 2112)));
-      userService.add(new User("User6", "Lastname6", "user6@mail.ru", new Car("Волга", 21)));
-      userService.add(new User("User7", "Lastname7", "user7@mail.ru", new Car("BMW", 525)));
-      userService.add(new User("User8", "Lastname8", "user8@mail.ru", new Car("Porshe", 916)));
-
-       users = userService.listUsers();
-      for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
-         System.out.println();
-      }
-
-
-      context.close();
-   }
+        users = userService.getUserByCar("Волга", 21);
+        for (User user : users){
+            System.out.println(user);
+        }
+        context.close();
+    }
 }
